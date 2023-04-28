b1aa8d5bc14a760a8a8453f7d3dc9196f84f92c3
Alan Rios <alanrios2001@gmail.com>
2023-04-27 22:37:37 -0300

    improved match

--- OIE/datasets/src/match.py
+++ OIE/datasets/src/match.py
@@ -1,10 +1,8 @@
 import os
 import json
 import spacy
-from spacy.matcher import Matcher, PhraseMatcher
-from spacy.symbols import ORTH
+from spacy.matcher import Matcher
 from tqdm import tqdm
-import re
 
 
 class OIE_Match:
@@ -21,10 +19,6 @@ class OIE_Match:
             os.system("python -m spacy download pt_core_news_lg")
             self.nlp = spacy.load("pt_core_news_lg")
 
-    def remove_special_case(self, tokenizer, special_case):
-        # Remova a entrada do dicionário special_cases se ela existir
-        if special_case in tokenizer.special_cases:
-            del tokenizer.special_cases[special_case]
     def validate_ext(self, sequential):
 
         json_dir = self.path_dir + "/json_dump.json"
@@ -32,94 +26,101 @@ class OIE_Match:
             data = json.load(f)
 
         for key in tqdm(range(len(data)), desc="Carregando dados"):
-            cases = []
+
             key = str(key)
-            raw_sent = data[key]["sent"]
-            raw_sent = raw_sent.replace("`", "").replace("'", "")
-            raw_sent = re.sub(r'\u200b', '', raw_sent)
-
-            sent1 = self.nlp(raw_sent)
-            raw_sent2 = [token.text for token in sent1]
-            if raw_sent2[-1] != ".":
-                raw_sent2.append(".")
-            raw_sent2_splited = raw_sent2
-            raw_sent2 = " ".join(raw_sent2)
-            raw_sent2 = raw_sent2.replace(" ,", ",")
-            raw_sent2 = raw_sent2.replace(" .", ".")
-            sent2 = self.nlp(raw_sent2)
-            for token in sent2:
-                if token.pos_ == "PROPN" and "." in token.text:
-                    abreviation = [{"ORTH": str(token.text).lower()}]
-                    self.nlp.tokenizer.add_special_case(str(token.text).lower(), abreviation)
-                    cases.append(str(token.text).lower())
-
-            sent = raw_sent2.lower()
-            sentence = self.nlp(sent)
-
+            sentence = self.nlp(data[key]["sent"].lower())
             ext = data[key]["ext"][0]
 
-            arg1 = ext["arg1"]
-            rel = ext["rel"]
-            arg2 = ext["arg2"]
-            arg1 = arg1.replace("`", "").replace("'", "")
-            rel = rel.replace("`", "").replace("'", "")
-            arg2 = arg2.replace("`", "").replace("'", "")
+            arg1 = ext["arg1"].lower().split(" ")
+            rel = ext["rel"].lower().split(" ")
+            arg2 = ext["arg2"].lower().split(" ")
 
-            arg1 = re.sub(r'\u200b', '', arg1)
-            arg2 = re.sub(r'\u200b', '', arg2)
-            rel = re.sub(r'\u200b', '', rel)
-            if len(arg2) > 0 and arg2[-1] == ".":
-                arg2 = arg2[:-1]
+            arg1_str = list(filter(None, arg1))
+            rel_str = list(filter(None, rel))
+            arg2_str = list(filter(None, arg2))
 
-            arg1 = self.nlp(arg1.lower())
-            rel = self.nlp(rel.lower())
-            arg2 = self.nlp(arg2.lower())
+            arg1 = [self.nlp.make_doc(text) for text in arg1_str]
+            rel = [self.nlp.make_doc(text) for text in rel_str]
+            arg2 = [self.nlp.make_doc(text) for text in arg2_str]
 
             # encontrar arg1
-            arg1_matcher = PhraseMatcher(self.nlp.vocab)
-            arg1_matcher.add("arg1", [arg1])
+            arg1_matcher = Matcher(self.nlp.vocab)
+            # cria o padrão de busca para o arg1
+            pattern = []
+            for token in arg1:
+                pattern.append({"LOWER": token.text})
+            if len(pattern) > 0:
+                arg1_matcher.add("arg1", [pattern])
+            # faz a busca pelo match
             arg1_match = arg1_matcher(sentence)
-            #print("arg1_match")
-            #print(arg1)
-            #print(arg1_match)
-
-            # encontrar arg2
-            arg2_matcher = PhraseMatcher(self.nlp.vocab)
-            arg2_matcher.add("arg2", [arg2])
-            arg2_match = arg2_matcher(sentence)
-            #print("arg2_match")
-            #print(arg2)
-            #print(arg2_match)
+            # padroniza o match
+            try:
+                arg1_match = (arg1_match[0][1], arg1_match[0][2] - 1)
+            except:
+                arg1_match = ()
 
             # encontrar relações
-            rel_matcher = PhraseMatcher(self.nlp.vocab)
-            rel_matcher.add("rel", [rel])
+            rel_matcher = Matcher(self.nlp.vocab)
+            # cria o padrão de busca para o rel
+            pattern = []
+            for token in rel:
+                pattern.append({"LOWER": token.text})
+            if len(pattern) > 0:
+                rel_matcher.add("rel", [pattern])
+            # faz a busca pelo match
             rel_match = rel_matcher(sentence)
-            #print("rel_match")
-            #print(rel)
-            #print(rel_match)
+            # se houver mais de um match, seleciona o que está após o arg1
+            if len(rel_match) > 1 and len(arg1_match) > 0:
+                for match in rel_match:
+                    if match[1] > arg1_match[1]:
+                        rel_match = [match]
+                        break
+            # padroniza o match
+            try:
+                rel_match = (rel_match[0][1], rel_match[0][2] - 1)
+            except:
+                rel_match = ()
+
+            # encontrar arg2
+            arg2_matcher = Matcher(self.nlp.vocab)
+            # cria o padrão de busca para o arg2
+            pattern = []
+            for token in arg2:
+                pattern.append({"LOWER": token.text})
+            if len(pattern) > 0:
+                arg2_matcher.add("arg2", [pattern])
+            arg2_match = arg2_matcher(sentence)
+            # se houver mais de um match, seleciona o que está após a relação
+            if len(arg2_match) > 1 and len(rel_match) > 0:
+                for match in arg2_match:
+                    if match[1] > rel_match[1]:
+                        arg2_match = [match]
+                        break
+            # padroniza o match
+            try:
+                arg2_match = (arg2_match[0][1], arg2_match[0][2] - 1)
+            except:
+                arg2_match = ()
+
 
             # select valid extractions
             if len(arg1_match) > 0 and len(rel_match) > 0 and len(arg2_match) > 0:
                 if sequential:
-                    if arg1_match[0][2] < rel_match[0][2] < arg2_match[0][2]:
-                        self.valid[raw_sent2] = {
-                            "sent": raw_sent2_splited,
+                    if arg1_match[1] < rel_match[0] and rel_match[1] < arg2_match[0]:
+                        self.valid[data[key]["sent"]] = {
                             "arg1": arg1_match,
                             "arg2": arg2_match,
                             "rel": rel_match,
                         }
-                        sent = self.nlp(raw_sent2)
+                        sent = self.nlp(data[key]["sent"])
                         tk = [token.text for token in sent]
-                        self.valid_data[raw_sent2] = {
-                            "sent": raw_sent2_splited,
-                            "arg1": (tk[arg1_match[0][1]:arg1_match[0][2]]),
-                            "rel": (tk[rel_match[0][1]:rel_match[0][2]]),
-                            "arg2": (tk[arg2_match[0][1]:arg2_match[0][2]]),
+                        self.valid_data[data[key]["sent"]] = {
+                            "arg1": (tk[arg1_match[0]:arg1_match[1]]),
+                            "rel": (tk[rel_match[0]:rel_match[1]]),
+                            "arg2": (tk[arg2_match[0]:arg2_match[1]]),
                         }
                 elif (arg1_match[0][2] < rel_match[0][2] < arg2_match[0][2]) == False:
-                    self.valid[raw_sent2] = {
-                        "sent": raw_sent2_splited,
+                    self.valid[data[key]["sent"]] = {
                         "arg1": arg1_match,
                         "arg2": arg2_match,
                         "rel": rel_match,
@@ -139,36 +140,25 @@ class OIE_Match:
                     rel_tuple = (rel_match[0][1], rel_match[0][2])
                 except:
                     rel_tuple = (0, 0)
-                sent = self.nlp(raw_sent2)
+                sent = self.nlp(data[key]["sent"])
                 tk = [token.text for token in sent]
 
                 try:
-                    self.invalid[raw_sent2] = {
+                    self.invalid[data[key]["sent"]] = {
                         "ID": data[key]["ID"],
-                        "expected": {
-                            "arg1": arg1.text,
-                            "rel": rel.text,
-                            "arg2": arg2.text,
-                        },
-                        "sent": raw_sent2,
+                        "expected": ext,
                         "arg1": (arg1_tuple[0], arg1_tuple[1], tk[arg1_tuple[0]:arg1_tuple[1]]),
                         "rel": (rel_tuple[0], rel_tuple[1], tk[rel_tuple[0]:rel_tuple[1]]),
                         "arg2": (arg2_tuple[0], arg2_tuple[1], tk[arg2_tuple[0]:arg2_tuple[1]]),
                     }
                 except:
-                    self.invalid[raw_sent2] = {
+                    self.invalid[data[key]["sent"]] = {
                         "ID": key,
-                        "expected": {
-                            "arg1": arg1.text,
-                            "rel": rel.text,
-                            "arg2": arg2.text,
-                        },
+                        "expected": ext,
                         "arg1": (arg1_tuple[0], arg1_tuple[1], tk[arg1_tuple[0]:arg1_tuple[1]]),
                         "rel": (rel_tuple[0], rel_tuple[1], tk[rel_tuple[0]:rel_tuple[1]]),
                         "arg2": (arg2_tuple[0], arg2_tuple[1], tk[arg2_tuple[0]:arg2_tuple[1]]),
                     }
-        for case in cases:
-            self.nlp.tokenizer.remove_special_case(self.nlp, case)
 
         with open(self.path_dir+"/invalid.json", "a", encoding="utf-8") as f:
             json.dump(self.invalid, f, ensure_ascii=False, indent=4)
@@ -180,58 +170,53 @@ class OIE_Match:
     def create_corpus(self):
         with open(f"{self.path_dir}/{self.output_name}_corpus.txt", "a", encoding="utf-8") as file:
             for sent in tqdm(self.valid, desc="Criando conll"):
-                sent_tokens = self.valid[sent]["sent"]
-                arg1_spans = []
-                arg2_spans = []
-                rel_spans = []
-                for match in self.valid[sent]["arg1"]:
-                    arg1_spans.append((match[1], match[2]))
-                for match in self.valid[sent]["arg2"]:
-                    arg2_spans.append((match[1], match[2]))
-                for match in self.valid[sent]["rel"]:
-                    rel_spans.append((match[1], match[2]))
+                sentence = self.nlp(sent)
+                sent_tokens = [token.text for token in sentence]
+                arg1_spans = [self.valid[sent]["arg1"]]
+                arg2_spans = [self.valid[sent]["arg2"]]
+                rel_spans = [self.valid[sent]["rel"]]
 
                 label_lines = ""
                 for i in range(len(sent_tokens)):
-                    if i >= arg1_spans[0][0] and i < arg1_spans[0][1]:
-                        if i == arg1_spans[0][0] and arg1_spans[0][1] - arg1_spans[0][0] == 1:
+                    if i >= arg1_spans[0][0] and i <= arg1_spans[0][1]:
+                        if i == arg1_spans[0][0] and arg1_spans[0][1] == arg1_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tS-ARG0\t-"
                             label_lines += line + "\n"
-                        elif i == arg1_spans[0][0] and arg1_spans[0][1] - arg1_spans[0][0] > 1:
+                        elif i == arg1_spans[0][0] and arg1_spans[0][1] != arg1_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tB-ARG0\t-"
                             label_lines += line + "\n"
-                        elif i > arg1_spans[0][0] and i < arg1_spans[0][1] - 1:
+                        elif i > arg1_spans[0][0] and i < arg1_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tI-ARG0\t-"
                             label_lines += line + "\n"
-                        elif i == arg1_spans[0][1] - 1:
+                        elif i == arg1_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tE-ARG0\t-"
                             label_lines += line + "\n"
 
-                    elif i >= arg2_spans[0][0] and i < arg2_spans[0][1]:
-                        if i == arg2_spans[0][0] and arg2_spans[0][1] - arg2_spans[0][0] == 1:
+                    elif i >= arg2_spans[0][0] and i <= arg2_spans[0][1]:
+                        if i == arg2_spans[0][0] and arg2_spans[0][1] == arg2_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tS-ARG1\t-"
                             label_lines += line + "\n"
-                        elif i == arg2_spans[0][0] and arg2_spans[0][1] - arg2_spans[0][0] > 1:
+                        elif i == arg2_spans[0][0] and arg2_spans[0][1] != arg2_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tB-ARG1\t-"
                             label_lines += line + "\n"
-                        elif i > arg2_spans[0][0] and i < arg2_spans[0][1] - 1:
+                        elif i > arg2_spans[0][0] and i < arg2_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tI-ARG1\t-"
                             label_lines += line + "\n"
-                        elif i == arg2_spans[0][1] - 1:
+                        elif i == arg2_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tE-ARG1\t-"
                             label_lines += line + "\n"
 
-                    elif i >= rel_spans[0][0] and i < rel_spans[0][1]:
-                        if i == rel_spans[0][0] and rel_spans[0][1] - rel_spans[0][0] == 1:
+                    elif i >= rel_spans[0][0] and i <= rel_spans[0][1]:
+                        if i == rel_spans[0][0] and rel_spans[0][1] == rel_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tS-V\t-"
                             label_lines += line + "\n"
-                        elif i == rel_spans[0][0] and rel_spans[0][1] - rel_spans[0][0] > 1:
+                        elif i == rel_spans[0][0] and rel_spans[0][1] != rel_spans[0][0]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tB-V\t-"
                             label_lines += line + "\n"
-                        elif i > rel_spans[0][0] and i < rel_spans[0][1] - 1:
+                        elif i > rel_spans[0][0] and i < rel_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tI-V\t-"
                             label_lines += line + "\n"
-                        elif i == rel_spans[0][1] - 1:
+                        elif i == rel_spans[0][1]:
                             line = f"{sent_tokens[i]}\tXX\t-\t-\t-\t-\t-\t*\tE-V\t-"
                             label_lines += line + "\n"
 
