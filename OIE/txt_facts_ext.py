37a1e9609dba9337dda27737bf78c4c568a3f62c
Alan Rios <66891368+alanrios2001@users.noreply.github.com>
2023-08-13 15:20:30 -0300

    Update txt_facts_ext.py

--- OIE/txt_facts_ext.py
+++ OIE/txt_facts_ext.py
@@ -1,33 +1,49 @@
 from predict import Predictor
+from OIE.datasets.feedback_dataset import FeedBackDataset
 from OIE.datasets.validated_splits.contractions import transform_portuguese_contractions
-from openie_helper.triple import OIESentence
 
-model = "TA_bertina"
+model = "TA"
 oie = Predictor(model)
 show_triple = True
+fb = FeedBackDataset()
 
 with open("texto.txt", "r", encoding="utf-8") as f:
     lines = f.read()
-    lines = lines.replace("\n\n", " ")
-    #lines = lines.replace("Dr. ", "Dr.")
-    lines = lines.split(". ")[:-1]
+    lines = lines.replace("\n", "")
+    lines = lines.split(".")[:-1]
 
 exts = []
-raw_exts = []
 for line in lines:
     ext = oie.pred(transform_portuguese_contractions(line), False)
-    raw_exts.append(ext)
     for e in ext:
-        ex = []
+        ex = [transform_portuguese_contractions(line)]
         for i in e:
             ex.append(i)
-        exts.append((transform_portuguese_contractions(line), ex))
+        exts.append(ex)
 
-print('quantidade de extrações: ', len(exts))
-print("-----------------------------------")
+print('quantidade de extrações:', len(exts))
+print('-'*50)
+displayed_extractions = []
 for i, ex in enumerate(exts):
-    extraction = f"{i} - extração: "
-    if show_triple:
-        print(extraction + " → " + f"(ARG0: {ex[1][0][0]})" + f"(REL: {ex[1][1][0]})" + f"(ARG1: {ex[1][2][0]})")
-    else:
-        print(extraction)
+    lenght = len(ex)
+    extraction = f"{i} - extração:"
+    if lenght > 2:
+        displayed_extractions.append([transform_portuguese_contractions(ex[0]),
+                                      transform_portuguese_contractions(ex[1][0]),
+                                      transform_portuguese_contractions(ex[2][0]),
+                                      transform_portuguese_contractions(ex[3][0])])
+        for i in range(1, lenght):
+            extraction += f" {ex[i][0]}"
+        if show_triple:
+            print(extraction + " → " + f"(ARG0: {ex[1][0]})" + f"(REL: {ex[2][0]})" + f"(ARG1: {ex[3][0]})")
+        else:
+            print(extraction)
+
+
+#valid_idx = input('selecione as extrações corretas pelo número da extração, separados por vírgula ex:(0, 2, 4): ')
+valid_idx = '0, 1, 2, 3, 4, 5, 6, 7, 8'
+valid_idx = valid_idx.split(', ')
+valid_idx = [int(i) for i in valid_idx]
+for i in valid_idx:
+    fb.main(displayed_extractions[i][0], displayed_extractions[i][1], displayed_extractions[i][2], displayed_extractions[i][3])
+
