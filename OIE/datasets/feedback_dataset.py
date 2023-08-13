f5f10432390afcbe37179d82847411ab743f0b79
Alan Rios <66891368+alanrios2001@users.noreply.github.com>
2023-08-13 15:20:56 -0300

    Update feedback_dataset.py

--- OIE/datasets/feedback_dataset.py
+++ OIE/datasets/feedback_dataset.py
@@ -2,17 +2,24 @@ import pathlib
 import re
 import os
 from final import matcher
+import spacy
 
 class FeedBackDataset:
     def __init__(self):
         self.dir = "feedback"
         self.data_dict = {}
         self.matcher = matcher.OIE_Match()
+        self.nlp = spacy.load("pt_core_news_lg")
         path = pathlib.Path(self.dir)
         path.mkdir(parents=True, exist_ok=True)
 
 
     def main(self, sentence, arg0, rel, arg1):
+        print('sentence:', sentence)
+        print('arg0:', arg0)
+        print('rel:', rel)
+        print('arg1:', arg1)
+
         dataset = self.open_dataset()
         self.extruct_dataset(dataset)
         new_data = self.extruct_new_data(sentence, arg0, rel, arg1)
@@ -22,22 +29,51 @@ class FeedBackDataset:
 
     def save_data(self):
         file_text = ""
-        for key in self.data_dict.keys():
+        dict = self.data_dict.keys()
+        dict = list(dict)[-1]
+        for key in [dict]:
             match = self.matcher.match(key, self.data_dict[key][0], self.data_dict[key][1], self.data_dict[key][2])
             if match[-1]:
-                for i,token in enumerate(key.split(" ")):
+                for i,token in enumerate([token.text for token in self.nlp(key)]):
                     if i in range(match[0][0], match[0][1]+1):
-                        file_text += token + "\t" + "ARG0" + "\n"
+                        if len(range(match[0][0], match[0][1]+1)) == 1:
+                            file_text += token + "\t" + "S-ARG0" + "\n"
+                        elif i == match[0][0]:
+                            file_text += token + "\t" + "B-ARG0" + "\n"
+                        elif i > match[0][0] and i<match[0][1]:
+                            file_text += token + "\t" + "I-ARG0" + "\n"
+                        elif i == match[0][1]:
+                            file_text += token + "\t" + "E-ARG0" + "\n"
+
                     elif i in range(match[1][0], match[1][1]+1):
-                        file_text += token + "\t" + "V" + "\n"
+                        if len(range(match[1][0], match[1][1] + 1)) == 1:
+                            file_text += token + "\t" + "S-V" + "\n"
+                        elif i == match[1][0]:
+                            file_text += token + "\t" + "B-V" + "\n"
+                        elif i > match[1][0] and i < match[1][1]:
+                            file_text += token + "\t" + "I-V" + "\n"
+                        elif i == match[1][1]:
+                            file_text += token + "\t" + "E-V" + "\n"
+
                     elif i in range(match[2][0], match[2][1]+1):
-                        file_text += token + "\t" + "ARG1" + "\n"
+                        if len(range(match[2][0], match[2][1] + 1)) == 1:
+                            file_text += token + "\t" + "S-ARG1" + "\n"
+                        elif i == match[2][0]:
+                            file_text += token + "\t" + "B-ARG1" + "\n"
+                        elif i > match[2][0] and i < match[2][1]:
+                            file_text += token + "\t" + "I-ARG1" + "\n"
+                        elif i == match[2][1]:
+                            file_text += token + "\t" + "E-ARG1" + "\n"
                     else:
                         file_text += token + "\t" + "O" + "\n"
                 file_text += "\n"
-        os.remove(self.dir + "/fb_dataset.txt")
         with open(self.dir + "/fb_dataset.txt", "a", encoding="utf-8") as f:
-            f.write(file_text)
+            try:
+                txt_f = f.read()
+            except:
+                txt_f = ""
+            txt_f += file_text
+            f.write(txt_f)
 
 
 
@@ -63,8 +99,12 @@ class FeedBackDataset:
         return new_data
 
     def open_dataset(self):
-        with open(self.dir + "/fb_dataset.txt", "r", encoding="utf-8") as f:
-            return f.read()
+        try:
+            with open(self.dir + "/fb_dataset.txt", "r", encoding="utf-8") as f:
+                return f.read()
+        except:
+            with open(self.dir + "/fb_dataset.txt", "w", encoding="utf-8") as f:
+                return ''
 
     def extruct_dataset(self, dataset):
         dataset = dataset.split("\n\n")
@@ -82,8 +122,12 @@ class FeedBackDataset:
                     rel += element.split("\t")[0] + " "
                 elif "ARG1" in element:
                     arg1 += element.split("\t")[0] + " "
+            sent = sent[0:-1]
+            arg0 = arg0[0:-1]
+            rel = rel[0:-1]
+            arg1 = arg1[0:-1]
             self.data_dict[sent] = [arg0, rel, arg1]
 
 
-fb = FeedBackDataset()
-fb.main("O Brasil é um país tropical", "O Brasil", "é", "um país tropical")
+#fb = FeedBackDataset()
+#fb.main("O Brasil é um país tropical", "O Brasil", "é", "um país tropical")
